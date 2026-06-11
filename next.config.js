055cd1ee4d0983c6806c5a55f445d0d7e9f3706d
goldendevuz <goldendevuz@gmail.com>
2026-06-11 20:04:34 +0500

    2026.06.11/20:04

--- next.config.js
+++ next.config.js
@@ -1,27 +1,14 @@
 /** @type {import('next').NextConfig} */
 
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  cacheStartUrl: false,
-  dynamicStartUrl: true,
-  runtimeCaching: [],
-  disable: process.env.NODE_ENV === 'development',
-});
-
-const nextConfig = withPWA({
+module.exports = {
   reactStrictMode: true,
+
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
-        port: '',
-        search: '',
       },
     ],
   },
-});
-
-module.exports = nextConfig;
+};
